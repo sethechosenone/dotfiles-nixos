5e4faf443a3343912f9889fd7f6f1c101b3c8c63
Seth Adkins <seth.adkins@protonmail.com>
2025-08-13 19:00:56 -0400

    switch off nixos-unstable branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "System config for all my NixOS systems";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager/master";
